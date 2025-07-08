e8b8cbca7bd65a492fcb1790ba22a99dc75e2a0c
AndresDuran53 <andresduranrod@gmail.com>
2025-07-08 02:06:54 -0600

    Using ESP32RMTLEDStripLightOutput directly

--- components/esp32_wled_adapter/esp32_wled_adapter.cpp
+++ components/esp32_wled_adapter/esp32_wled_adapter.cpp
@@ -1,5 +1,6 @@
 #include "esp32_wled_adapter.h"
 #include "esphome/core/log.h"
+#include "esphome/components/esp32_rmt_led_strip/led_strip.h" // Incluir el encabezado para ESP32RMTLEDStripLightOutput
 #include <lwip/sockets.h>
 #include <errno.h>
 
@@ -37,8 +38,8 @@ void WLEDUDPComponent::loop() {
   auto light_output = this->light_strip_->get_output();
   if (light_output == nullptr) return;
 
-  auto addressable_light = static_cast<light::AddressableLight*>(light_output);
-  if (addressable_light == nullptr) return;
+  auto led_strip = static_cast<esp32_rmt_led_strip::ESP32RMTLEDStripLightOutput*>(light_output);
+  if (led_strip == nullptr) return;
 
   uint8_t udp_buffer[2048];
   struct sockaddr_in sender_address;
@@ -49,18 +50,29 @@ void WLEDUDPComponent::loop() {
   if (received_bytes < 0) return;
 
   // Parse received data as RGB triplets
-  int led_count = addressable_light->size();
+  int led_count = led_strip->size();
   int max_possible_leds = received_bytes / 3;
   int leds_to_update = std::min(led_count, max_possible_leds);
 
   for (int led_index = 0; led_index < leds_to_update; led_index++) {
     int buffer_position = led_index * 3;
-    Color led_color(udp_buffer[buffer_position], 
-                    udp_buffer[buffer_position + 1], 
-                    udp_buffer[buffer_position + 2]);
-    addressable_light->set_pixel_at(led_index, led_color);
+    
+    // Obtener una vista directa al LED usando el método específico de ESP32RMTLEDStripLightOutput
+    auto color_view = led_strip->get_view_internal(led_index);
+    
+    // Establecer valores RGB directamente
+    *color_view.red = udp_buffer[buffer_position];
+    *color_view.green = udp_buffer[buffer_position + 1]; 
+    *color_view.blue = udp_buffer[buffer_position + 2];
+    
+    // Si hay un canal blanco disponible, lo dejamos en 0
+    if (color_view.white != nullptr) {
+      *color_view.white = 0;
+    }
   }
-  addressable_light->schedule_show();
+  
+  // Programar la actualización de la tira LED
+  led_strip->schedule_show();
 }
 
 }  // namespace esp32_wled_adapter
